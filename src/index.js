4bbb73c82eed179824a67f9ed51fe291a0fbeb2b
Joao Jorge <tech.joaojorge@gmail.com>
2024-08-22 15:22:24 +0100

    Some changes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// Access environment variables
+const DBURL = process.env.DATABASE_URL;
+const port = process.env.PORT || 2000;
 
 const app = express();
 app.use(bodyParser.json());
 
-let votes = {
-    'Option A': 0,
-    'Option B': 0,
-    'Option C': 0
-};
 
-// Endpoint to get voting options
-app.get('/options', (req, res) => {
-    res.json(Object.keys(votes));
-});
 
-// Endpoint to submit a vote
-app.post('/vote', (req, res) => {
-    const { option } = req.body;
-    if (votes.hasOwnProperty(option)) {
-        votes[option]++;
-        res.json({ message: 'Vote counted' });
-    } else {
-        res.status(400).json({ message: 'Invalid option' });
-    }
-});
-
-// Endpoint to get results
-app.get('/results', (req, res) => {
-    res.json(votes);
-});
-
-const port = 3000;
+console.log("porta: ", port)
+console.log("porta: ", process.env.DATABASE_URL)
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
