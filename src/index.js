544c325f34725753750c7b196b6135c2a4558c97
Joao Jorge <tech.joaojorge@gmail.com>
2024-08-22 15:32:39 +0100

    Solve the dotenv variable

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const bodyParser = require('body-parser');
 
 // Access environment variables
 const DBURL = process.env.DATABASE_URL;
-const port = process.env.PORT || 2000;
+const port = process.env.PORT;
 
 const app = express();
 app.use(bodyParser.json());
 
-console.log("porta: ", port)
-console.log("porta: ", process.env.DATABASE_URL)
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
