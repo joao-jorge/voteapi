91c1bbb8c746449b868bbf42662bab36a73dfcf5
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-25 14:47:46 +0000

    save

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -3,28 +3,25 @@ const router = express.Router()
 
 const User = require('../models/user'); // 
 
-
 // List all users
 router.get('/', async (req, res) => {
     try {
-      const items = await User.find(); // Fetch all items from the collection
-      res.status(200).json(items); // Send the items as JSON response
+      const items = await User.find(); 
+      res.status(200).json(items); 
     } catch (err) {
       res.status(500).json({ message: 'Server error', error: err });
     }
 });
 
 // Show a user
-router.post('/user/?', (req, res) => {
-  res.send('Route to show a user')
-})
+
 // Create a user
-router.post('/users', async (req, res) => {
+router.post('/user', async (req, res) => {
   try {
     const { name, email, password } = req.body;
-
+    
     // Create a new user instance
-    const user = new user({ name, email, password });
+    const user = new User({ name, email, password });
     
     // Save the user to the database
     await user.save();
--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config({ path: '.env' });
 
-const uri = "mongodb+srv://joao:nuno12@mongoclusterlearn.z3vxm.mongodb.net/?retryWrites=true&w=majority&appName=mongoclusterlearn";
+const uri = "mongodb+srv://joao:nuno12@mongoclusterlearn.z3vxm.mongodb.net/voteapi_db?retryWrites=true&w=majority&appName=mongoclusterlearn";
 //mongodb+srv://joao:nuno12@mongoclusterlearn.z3vxm.mongodb.net/?retryWrites=true&w=majority&appName=mongoclusterlearn
 
 mongoose.connect(uri, {
