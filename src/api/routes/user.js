2adcd3b392930989266e47fab69dcdcb73dea60a
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-26 10:21:46 +0000

    sa

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -3,26 +3,8 @@ const router = express.Router()
 
 const User = require('../models/user'); // 
 
-// List all users
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.find(); 
-    res.status(200).json(users); 
-  } catch (error) { res.status(500).json({ message: error.message }); }
-});
-
-// Show a user
-router.get('/user/:id', async(req, res) =>{
-  try {
-    const {id} = req.params.id;
-    const user = await User.findById(id);
-    res.status(200).json(user);
-  } catch (error) { res.status(500).json({message: req.params.id}); }
-})
-
-
 // Create a user
-router.post('/user', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const user = new User({ name, email, password });
@@ -31,11 +13,37 @@ router.post('/user', async (req, res) => {
   } catch (error) { res.status(500).json({ message: error.message }); }
 });
 
+
+
+
+
+/*
+
+// List all users
+router.get('/user', async (req, res) => {
+  try {
+    const users = await User.find(); 
+    res.status(200).json(users); 
+  } catch (error) { res.status(500).json({ message: error.message }); }
+});
+
+// Show a user
+router.get('/:id', async(req, res) =>{
+  try {
+    const {id} = req.params.id;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) { res.status(500).json({message: req.params.id}); }
+})
+
+
+
+
 // Update user's information
 
 
 // Delete a user
-
+*/
 
   
 module.exports = router
