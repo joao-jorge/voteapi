e5cdf28b31271a2181313c0b79fcf4615c53b560
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-26 09:43:55 +0000

    refactor get user endpoint

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -17,9 +17,7 @@ router.get('/user/:id', async(req, res) =>{
     const {id} = req.params;
     const user = await User.findById(id);
     res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({message: ""})
-  }
+  } catch (error) { res.status(500).json({message: error.message}); }
 })
 
 
