4cca46b84be709f2168ce890b6998624b1e49085
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-26 23:19:38 +0000

    simple change

--- src/api/routes/election.js
+++ src/api/routes/election.js
@@ -4,7 +4,7 @@ const Election = require('../controllers/election')
 
 Route.post('/', Election.createElection);
 Route.get('/:id', Election.getElection);
-Route.get('/', Election.lisAll);
+Route.get('/', Election.listAll);
 Route.delete('/:id', Election.deleteElection)
 
 module.exports = Route
