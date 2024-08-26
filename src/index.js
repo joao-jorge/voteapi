d2f73faefda373487e162b9e0423b9e152d5bcdc
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-26 08:46:14 +0000

    refactor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
-const Router = express.Router();
 require('dotenv').config({ path: '../.env' });
 
 // Database connection
