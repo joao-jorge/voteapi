fa06d8a29677f1bb20b53c098cb6c606c44273c5
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-26 23:05:33 +0000

    create an election route

---
+++ src/api/routes/election.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const Route = express.Router()
+const Election = require('../controllers/election')
+
+Route.post('/', Election.createElection);
+Route.get('/:id', Election.getElection);
+Route.get('/', Election.lisAll);
+Route.delete('/:id', Election.deleteElection)
+
+module.exports = Route
