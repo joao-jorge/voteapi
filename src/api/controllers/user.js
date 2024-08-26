793a76a1b626feebac152f5a7bcf93e8bd9fdef4
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-26 12:42:04 +0000

    changes on controllers and routes

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 
-const createUser = async (req, res) => {
+const create = async (req, res) => {
     try {
       const { name, email, password } = req.body;
       const user = new User({ name, email, password });
@@ -9,6 +9,47 @@ const createUser = async (req, res) => {
     } catch (error) { res.status(500).json({ message: error.message }); }
 }
 
+const list = async (req, res) => {
+    try {
+        const users = await User.find(); 
+        res.status(200).json(users); 
+    } catch (error) { res.status(500).json({ message: error.message }); }
+}
+
+const get = async(req, res) =>{
+    try {
+      const user = await User.findById(req.params.id);
+      res.status(200).json(user);
+    } catch (error) { res.status(500).json({message: error.message}); }
+}
+
+const remove = async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      const deleteUser = await User.findByIdAndDelete(user);
+      res.status(200).json({ message: 'User deleted successfully', deletedUser: deleteUser });
+    } catch (error) { res.status(500).json({ message: error.message }); }
+}
+
+const update = async(req, res) => {
+    try{
+      const { name, email, password } = req.body;
+      const user = await User.findById(req.params.id)
+      if(!user){
+        return res.status(404).json({message: "User not found"});
+      }
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, { name, email, password }, {new: true})
+      res.status(200).json({ message: 'User updated successfully', updatedUser: updatedUser })
+    } catch(error) {res.status(500).json({message: error.message})}
+}
+
 module.exports = {
-    createUser,
+    create,
+    list,
+    get,
+    remove,
+    update
 }
--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,49 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { createUser } = require('../controllers/user') 
+const userModel = require('../controllers/user') 
 
-// Create a user
-router.post('/', createUser);
-
-// List all users
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.find(); 
-    res.status(200).json(users); 
-  } catch (error) { res.status(500).json({ message: error.message }); }
-});
-
-// Get a user
-router.get('/:id', async(req, res) =>{
-  try {
-    const user = await User.findById(req.params.id);
-    res.status(200).json(user);
-  } catch (error) { res.status(500).json({message: error.message}); }
-});
-
-// Delete a user
-router.delete('/:id', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    const deleteUser = await User.findByIdAndDelete(user);
-    res.status(200).json({ message: 'User deleted successfully', deletedUser: deleteUser });
-  } catch (error) { res.status(500).json({ message: error.message }); }
-});
-
-// Update user's information
-router.put('/:id', async(req, res) => {
-  try{
-    const { name, email, password } = req.body;
-    const user = await User.findById(req.params.id)
-    if(!user){
-      return res.status(404).json({message: "User not found"});
-    }
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, { name, email, password }, {new: true})
-    res.status(200).json({ message: 'User updated successfully', updatedUser: updatedUser })
-  } catch(error) {res.status(500).json({message: error.message})}
-})
+router.post('/', userModel.create);
+router.get('/', userModel.list);
+router.get('/:id', userModel.get);
+router.delete('/:id', userModel.remove);
+router.put('/:id', userModel.update);
   
 module.exports = router
