b1faabf486fb021bf3e2353125827c145d1f0a0f
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-26 10:27:00 +0000

    change route name

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 // Routes 
 const userRoutes = require('./api/routes/user')
-app.use('/', userRoutes)
+app.use('/api/user', userRoutes)
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
