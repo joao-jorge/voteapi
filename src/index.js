5e7fc457c646d1e8add65e24df44e346b721dba0
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-26 22:27:33 +0000

    s

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(cors())
 // Routes 
 const userRoutes = require('./api/routes/user')
 app.use('/api/user', userRoutes)
-
+        
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
