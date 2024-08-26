95b9bd5ba9eab7179fec57fa799d52b493c7b194
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-26 09:41:19 +0000

    refactor user endpoint

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -8,9 +8,7 @@ router.get('/', async (req, res) => {
   try {
     const users = await User.find(); 
     res.status(200).json(users); 
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  } catch (error) { res.status(500).json({ message: error.message }); }
 });
 
 // Show a user
