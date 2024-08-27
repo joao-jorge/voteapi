750c9875b2f8e286c2fdd8807b2d89f3f5a6775c
Joao Jorge <tech.joaojorge@gmail.com>
2024-08-27 14:41:38 +0100

    small changes

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 require('dotenv').config({ path: '.env' });
 
-const uri = "mongodb+srv://joao:nuno12@mongoclusterlearn.z3vxm.mongodb.net/voteapi_db?retryWrites=true&w=majority&appName=mongoclusterlearn";
-//mongodb+srv://joao:nuno12@mongoclusterlearn.z3vxm.mongodb.net/?retryWrites=true&w=majority&appName=mongoclusterlearn
+const URI = process.env.LOCAL_URI || "mongodb+srv://joao:nuno12@mongoclusterlearn.z3vxm.mongodb.net/voteapi_db?retryWrites=true&w=majority&appName=mongoclusterlearn";
 
-mongoose.connect(uri, {
+mongoose.connect(URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
