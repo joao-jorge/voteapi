ba2800ff6c9297f8128cabd46e5be542b5e57b70
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-28 11:11:18 +0000

    update userModel

--- src/api/models/userModel.js
+++ src/api/models/userModel.js
@@ -2,22 +2,12 @@ const mongoose = require('mongoose');
 
 // Define the schema for a User
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-});
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "user"], default: "user" }
+},{ timestamps: true });
 
-// Create a model from the schema
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
