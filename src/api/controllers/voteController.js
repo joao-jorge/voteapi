1c29d9c2125aca8d850b1af7dd5ef11c69b7eb4b
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-30 20:42:58 +0000

    vote changes

--- src/api/controllers/voteController.js
+++ src/api/controllers/voteController.js
@@ -3,20 +3,24 @@ const Candidate = require('../models/candidateModel')
 const Election = require('../models/electionModel')
 const User = require('../models/userModel')
 
-
 const castVote = async (req, res) => {
     try{
         const { idElection, idUser, idCandidate } = req.params
 
-        const foundElection= await Election.findById(idElection);
+        // Input validation
+        if (!idElection || !idUser || !idCandidate) {
+          return res.status(400).json({ message: 'Invalid input parameters.' });
+        }
+                const foundElection= await Election.findById(idElection);
         if (!foundElection) {
           return res.status(404).json({ message: 'Could not vote! Election not found.' });
         }
 
         // Verify if the election is ongoing
-        //if(){
-
-        //}
+        const dateNow = new Date()
+        if(dateNow < foundElection.startDate || dateNow > foundElection.endDate){
+          return res.status(400).json({message: "The election is not available in this date"})
+        }
 
         // Verify if the candidate exists
         const foundCandidate = await Candidate.findById(idCandidate);
@@ -35,14 +39,15 @@ const castVote = async (req, res) => {
             return res.status(400).json({message: "Could not vote! User has already voted."})
         }
         
-    
         const vote = new Vote({
             election: idElection,
             candidate: idCandidate
         });
+        const savedVote = await vote.save();
 
-        console.log(idElection)
-        //const savedVote = await vote.save();
+        //Add the idElection to the user who has voted 
+        foundUser.election.push(idElection)
+        await foundUser.save()
         res.status(200).json({message: "Uer has successfully voted!"});
 
     }catch(error){res.status(500).json({ message: error.message })}
