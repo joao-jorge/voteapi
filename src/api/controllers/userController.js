eb1fdaec50f50be27bf82056f62dd9996c424c2a
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-08-31 20:27:21 +0000

    user controller

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,20 +1,23 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
+const mongoose = require('mongoose')
 
 const create = async (req, res) => {
     try {
       const { name, email, password, role } = req.body;
-      const hashedPassword = await bcrypt.hash(password, 10);
 
-      if(await findUserByEmail(email)){
-        return res.status(400).json({message: "User already exists" });
-      } 
+      if(!name || !email || !password)
+        return res.status(400).json({message: "Fill all the fields!"})
 
       // Verify is the data are inserted correctly
       const notValid = await validateInputs(name, email, password)
       if(notValid)
         return res.status(500).json({ message: notValid })
 
+      if(await findUserByEmail(email))
+        return res.status(400).json({message: "There is a user with this email" });
+
+      const hashedPassword = await bcrypt.hash(password, 10);
       const user = new User({ name, email, password: hashedPassword, role });
       await user.save();
       res.status(201).json({ message: "User created successfuly", user: user });
@@ -23,36 +26,59 @@ const create = async (req, res) => {
 
 const list = async (req, res) => {
     try {
-        const users = await User.find(); 
-        res.status(200).json(users); 
+        const foundUsers = await User.find(); 
+        if(!foundUsers)
+          return res.status(400).json({message: "There's no user."})
+
+        res.status(200).json({ foundUsers }); 
     } catch (error) { res.status(500).json({ message: error.message }); }
 }
 
 const get = async(req, res) =>{
     try {
-      const user = await User.findById(req.params.id);
-      res.status(200).json(user);
+      if (!mongoose.isValidObjectId(req.params.id)) 
+        return res.status(400).json({ message: "Invalid ID format!" });
+
+      const foundUser = await User.findById(req.params.id);
+      if(!foundUser)
+        return res.status(400).json({message: "User not found!"})
+
+      res.status(200).json(foundUser);
     } catch (error) { res.status(500).json({message: error.message}); }
 }
 
 const remove = async (req, res) => {
     try {
-      const user = await User.findById(req.params.id);
-      if (!user) {
+      if (!mongoose.isValidObjectId(req.params.id)) 
+        return res.status(400).json({ message: "Invalid ID format!" });
+
+      const foundUser = await User.findById(req.params.id);
+      if (!foundUser)
         return res.status(404).json({ message: 'User not found' });
-      }
-      const deleteUser = await User.findByIdAndDelete(user);
+    
+      const deleteUser = await User.findByIdAndDelete(foundUser);
       res.status(200).json({ message: 'User deleted successfully', deletedUser: deleteUser });
     } catch (error) { res.status(500).json({ message: error.message }); }
 }
 
 const update = async(req, res) => {
     try{
+      if (!mongoose.isValidObjectId(req.params.id)) 
+        return res.status(400).json({ message: "Invalid ID format!" });
+
       const { name, email, password } = req.body;
-      const user = await User.findById(req.params.id)
-      if(!user){
-        return res.status(404).json({message: "User not found"});
-      }
+      if(!name || !email || !password)
+        return res.status(400).json({message: "Fill all the fields!"});
+
+      // Verify is the data are inserted correctly
+      const notValid = await validateInputs(name, email, password)
+      if(notValid)
+        return res.status(500).json({ message: notValid })
+
+      const foundUser = await User.findById(req.params.id);
+      if (!foundUser) 
+        return res.status(404).json({ message: 'User not found' });
+
       const updatedUser = await User.findByIdAndUpdate(req.params.id, { name, email, password }, {new: true})
       res.status(200).json({ message: 'User updated successfully', updatedUser: updatedUser })
     } catch(error) {res.status(500).json({message: error.message})}
