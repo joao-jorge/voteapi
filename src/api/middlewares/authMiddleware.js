e62e21396f183e4c60170960136eecc28719a1c9
João Mateus Jorge <tech.joaojorge@gmail.com>
2024-09-06 22:58:25 +0000

    add authorization and authentication to user routes

--- src/api/middlewares/authMiddleware.js
+++ src/api/middlewares/authMiddleware.js
@@ -1,27 +1,37 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const User = require('../models/userModel')
 
-const authentication = (req, res, next) => {
+const authentication = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1]; // Bearer <token>
   
   if (!token) {
     return res.status(401).json({ message: 'No token provided' });
   }
 
-  jwt.verify(token, config.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ message: 'Invalid token' });
+  try {
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+    
+    // Fetch user data from the database
+    const user = await User.findById(decoded.userId).exec();
+    
+    if (!user) {
+      return res.status(401).json({ message: 'User not found' });
     }
-    req.user = decoded; // Attach user information to request object
+
+    // Attach full user data to the request object
+    req.user = user;
     next();
-  });
+  } catch (err) {
+    return res.status(403).json({ message: 'Invalid token' });
+  }
 };
 
 
 const authorization = (roles) => (req, res, next) => {
   if (!roles.includes(req.user.role)) {
-    return res.status(403).json({ message: 'Access denied' });
+    return res.status(403).json({ message: 'Access denied:'});
   }
   next();
 };
--- src/api/routes/userRoute.js
+++ src/api/routes/userRoute.js
@@ -1,18 +1,18 @@
 const express = require('express')
-const router = express.Router()
 const userModel = require('../controllers/userController') 
 const auth = require('../middlewares/authMiddleware')
+const router = express.Router()
 
 // Not protected routes
 router.post('/', userModel.create);
 router.post('/login', userModel.login);
 
 
-
-router.use(auth.authentication)
-router.get('/', userModel.list);
-router.get('/:id', userModel.get);
-router.delete('/:id', auth.authorization(['user']), userModel.remove);
-router.put('/:id', auth.authorization(['admin']), userModel.update);
+router.use(auth.authentication) // all routes under need auth
+// Protected routes
+router.get('/', auth.authorization(['admin']), userModel.list);
+router.get('/:id', auth.authorization(['admin']), userModel.get);
+router.delete('/:id', auth.authorization(['admin']), userModel.remove);
+router.put('/:id', auth.authorization(['admin', 'user']), userModel.update);
   
 module.exports = router
