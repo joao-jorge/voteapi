c1441632ab7dec1d20375c51b0c66dbd7475018c
Joao Jorge <tech.joaojorge@gmail.com>
2024-09-13 21:00:01 +0100

    con

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 require('dotenv').config({ path: '.env' });
@@ -8,9 +8,9 @@ const URI = process.env.LOCAL_URI || config.ONLINE_URL;
 mongoose.connect(URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
+});
 
 const connection = mongoose.connection;
 connection.once('open', ()=>{
   console.log('Connected to DB successfully');
-})
+});
